89501c1df48e6d633c6a2a4f1b4e1aeacf0ecea6
csutyak <cps46@zips.uakron.edu>
2021-09-06 23:47:59 -0400

    got rid of outdated code

--- lib/main.hpp
+++ lib/main.hpp
@@ -18,41 +18,4 @@ void greeting()
     std::cout << std::endl;
 }
 
-void outdated()
-{
-    int nodeCount;
-    std::cin >> nodeCount;
-
-    //validating user's input
-    while (std::cin.fail() || nodeCount < 0 || nodeCount > 100)
-    {
-        std::cin.clear();
-        std::cin.ignore(INT_MAX, '\n');
-        std::cout << "Invalid input. Must be a number." << std::endl;
-        std::cout << "Amount of nodes in graph: "; 
-        std::cin >> nodeCount;
-    }
-
-    //continue recieving input & organizing it
-    //Graph flowGraph;
-    for(int i = 0; i < nodeCount; i++)
-    {
-        std::string adjList;
-        std::cout << "Adjacency List for node " << i << ": ";
-        std::cin >> adjList;
-
-        if(adjList.compare("0"))
-        {
-            //there are no outgoing edges from this node
-            //flowGraph.addEdge(i, );
-        }
-        else
-        {
-            //read in the string and decifer between nodes and weights
-            //should also check user input to make sure it is valid
-            //flowGraph.addEdge(i, );
-        }
-    }
-}
-
 #endif
